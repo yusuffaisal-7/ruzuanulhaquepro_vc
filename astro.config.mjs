fb67383971c4ba59c94d54bb4a5fc725b00378e5
YusuF Faisal <yusufoesta7t@gmail.com>
2025-07-11 10:38:01 +0600

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify()
+  adapter: node({
+    mode: 'standalone'
+  })
 });
